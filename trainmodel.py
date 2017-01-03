51045009e0bb46d738c848c86fe0de21595582b8
BlueHelper <felipecastillo.atheist@gmail.com>
2017-01-03 16:22:49 -0300

    new trainmodel

--- trainmodel.py
+++ trainmodel.py
@@ -1,16 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from keras.models import Sequential
-from keras.layers import MaxoutDense, Convolution2D, pooling, MaxPooling2D, Flatten, Dense
+from keras.layers import MaxoutDense, Convolution2D, pooling, MaxPooling2D, Flatten, Dense, Dropout, Activation
 import pandas as pd
 import scipy as sp
 import numpy as np
 import glob
 from astropy.io import fits
 
-import tensorflow as tf
-from keras import backend as K
-K.set_session(tf.Session())
+#import tensorflow as tf
+#from keras import backend as K
+#K.set_session(tf.Session())
 
-# train data
+##### train data and labels(probabilities)
+#labels
 GZ2 = pd.read_csv('GalaxyZoo2.csv')
 r1 = GZ2["t01_smooth_or_features_a01_smooth_debiased"]
 r2 = GZ2["t01_smooth_or_features_a02_features_or_disk_debiased"]
@@ -25,10 +29,9 @@ for i in range(len(r1)):
     elif r3[i]==maxprob:
         prob[i] = 3
 
-
+#Train images
 files =  glob.glob("./kerasfits/*.fits")
 x_train = []
-#x_train = sp.array([])
 y_train = []
 for pathfile in files:
     hdulist = fits.open(pathfile)
@@ -49,31 +52,30 @@ x_train = sp.asarray(x_train)
 x_train = x_train.reshape((x_train.shape[0],1,x_train.shape[1],x_train.shape[2]))
 y_train = sp.asarray(y_train)
 
-
+##### CNN model
 
 model = Sequential()
-model.add(Convolution2D(16,10,10,border_mode='same',input_shape=(1,207,207)))
+model.add(Convolution2D(16,10,10,border_mode='same', activation='relu',input_shape=(1,207,207)))
 model.add(pooling.MaxPooling2D(pool_size=(2,2),border_mode='same')) #98x98
 
-model.add(Convolution2D(32,9,9,border_mode='same'))
+model.add(Convolution2D(32,9,9,border_mode='same', activation='relu'))
 model.add(pooling.MaxPooling2D(pool_size=(2,2),border_mode='same'))# 45x45
 
-model.add(Convolution2D(64,6,6,border_mode='same'))
+model.add(Convolution2D(64,6,6,border_mode='same', activation='relu'))
 model.add(pooling.MaxPooling2D(pool_size=(2,2),border_mode='same'))#20x20
 
-model.add(Convolution2D(128,5,5,border_mode='same'))
+model.add(Convolution2D(128,5,5,border_mode='same', activation='relu'))
 model.add(pooling.MaxPooling2D(pool_size=(2,2),border_mode='same')) #8x8
 
-model.add(Convolution2D(256,3,3,border_mode='same'))
-#6x6
+model.add(Convolution2D(256,3,3,border_mode='same', activation='relu'))#6x6
 
-model.add(Convolution2D(256,3,3,border_mode='same'))#4x4
-#model.add(MaxPooling2D(pool_size=(2,2),border_mode='same')) #2x2
+
+model.add(Convolution2D(256,3,3,border_mode='same', activation='relu'))#4x4
+model.add(MaxPooling2D(pool_size=(2,2),border_mode='same')) #2x2x256=1024
 model.add(Flatten())
+
 #model.add(MaxoutDense(2048))
+model.add(Dense(256))
 model.add(Dense(3)) #?
 model.compile(loss='mean_squared_error', optimizer='sgd')
-model.fit(x_train, y_train, batch_size=32, nb_epoch=1)
-
-#from sklearn.externals import joblib
-#joblib.dump(model,"model.pkl")
+model.fit(x_train, y_train, batch_size=32, nb_epoch=2)
