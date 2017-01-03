05d421ab0d6b55a9d39119b4221fd823a4fb8ab1
BlueHelper <felipecastillo.atheist@gmail.com>
2017-01-03 15:36:00 -0300

    generalized fetcher

--- fetcher.py
+++ fetcher.py
@@ -3,7 +3,13 @@ import wget as wg
 import pandas as pd
 import numpy as np
 import collections as cll
+import sys
+import os
+'''
+python fetcher.py numberofobject
+if numberofobject = 0 then all images will be downloaded
 
+'''
 GZ2 = pd.read_csv('GalaxyZoo2.csv')
 
 run = np.array(GZ2['run'].values, dtype=str)
@@ -13,7 +19,7 @@ field = np.array(GZ2['field'].values, dtype=str)
 
 aux_list = []
 
-#dic_aux = {}
+
 for i in range(len(GZ2)):
     #https://data.sdss.org/sas/dr13/eboss/photo/redux/157/1933/objcs/2/fpAtlas-001933-2-0011.fit
     k = 'https://data.sdss.org/sas/dr13/eboss/photo/redux/'+rerun[i]+'/'+run[i]+'/objcs/'+camcol[i]+'/fpAtlas-'+run[i].zfill(6)+'-'+camcol[i]+'-'+field[i].zfill(4)+'.fit'
@@ -22,16 +28,21 @@ for i in range(len(GZ2)):
 
 contador = cll.Counter(aux_list)
 
-atleast1000 = contador.most_common(100)
+todownload = int(sys.argv[1])
+if todownload==0:
+    todownload = len(contador)
+    atleastarg = contador.most_common(int(todownload))
+else:
+    atleastarg = contador.most_common(int(todownload/5))
 
-c = 0
+c = 0 #download counter
 
-for i in range(len(atleast1000)):
-    c+= atleast1000[i][1]
-
-    wg.download(atleast1000[i][0])
-    if c >= 1000:
+for i in range(len(atleastarg)):
+    c+= atleastarg[i][1]
+    wg.download(atleastarg[i][0])
+    if c >= todownload:
         print i
         break
-
-    #print atleast1000[i][0]
+if not os.path.exists(fit):
+    os.makedirs(fit)
+os.system('mv *.fit ./fit/')
